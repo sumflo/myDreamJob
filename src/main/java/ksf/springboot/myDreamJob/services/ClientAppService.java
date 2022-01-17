4476578d028f62d1add519426445d9922f931b51
sumflo <real61madrid@gmail.com>
2022-01-17 10:56:26 +0100

    register is service + modelmapper dep.

--- src/main/java/ksf/springboot/myDreamJob/services/ClientAppService.java
+++ src/main/java/ksf/springboot/myDreamJob/services/ClientAppService.java
@@ -1,7 +1,36 @@
 package ksf.springboot.myDreamJob.services;
 
+import ksf.springboot.myDreamJob.model.ClientApp;
+import ksf.springboot.myDreamJob.model.dto.ClientAppRequestDTO;
+import ksf.springboot.myDreamJob.repositories.ClientAppRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+// @RequiredArgsConstructor
 public class ClientAppService {
+
+    private final ClientAppRepository clientAppRepository;
+
+    @Autowired
+    public ClientAppService(ClientAppRepository clientAppRepository) {
+        this.clientAppRepository = clientAppRepository;
+    }
+
+    public void register(ClientAppRequestDTO clientAppRequestDTO) throws Exception{
+
+        if(clientAppRepository.findByEmail(clientAppRequestDTO.getEmail()).isPresent()){
+            throw new Exception();
+        }else{
+            ModelMapper modelMapper = new ModelMapper();
+            ClientApp clientApp = new ClientApp();
+            modelMapper.map(clientAppRequestDTO, clientApp);
+            clientAppRepository.save(clientApp);
+
+        }
+
+    }
+
 }
