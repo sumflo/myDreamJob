af6d02d7135301bfb4b80985c30f69eeee04a8f9
sumflo <real61madrid@gmail.com>
2022-01-17 09:08:24 +0100

    Initial commit

---
+++ src/main/java/ksf/springboot/myDreamJob/MyDreamJobApplication.java
@@ -0,0 +1,13 @@
+package ksf.springboot.myDreamJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDreamJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDreamJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ksf/springboot/myDreamJob/MyDreamJobApplicationTests.java
@@ -0,0 +1,13 @@
+package ksf.springboot.myDreamJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDreamJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
