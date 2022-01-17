cd7f352f2db7fac727e155d2d6eebe88558559f4
sumflo <real61madrid@gmail.com>
2022-01-17 10:47:13 +0100

    repos update with basics

--- src/main/java/ksf/springboot/myDreamJob/repositories/ClientAppRepository.java
+++ src/main/java/ksf/springboot/myDreamJob/repositories/ClientAppRepository.java
@@ -4,9 +4,12 @@ import ksf.springboot.myDreamJob.model.ClientApp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ClientAppRepository extends JpaRepository<ClientApp, UUID> {
 
     List<ClientApp> findAll();
+
+    Optional<ClientApp> findByEmail(String email);
 }
--- src/main/java/ksf/springboot/myDreamJob/repositories/JobAdvertisementRepository.java
+++ src/main/java/ksf/springboot/myDreamJob/repositories/JobAdvertisementRepository.java
@@ -5,10 +5,13 @@ import ksf.springboot.myDreamJob.model.JobAdvertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JobAdvertisementRepository extends JpaRepository<JobAdvertisement, UUID> { // or CrudRepository
 
     List<JobAdvertisement> findAll();
+
+    Optional<JobAdvertisement> findById(UUID id);
 
 }
