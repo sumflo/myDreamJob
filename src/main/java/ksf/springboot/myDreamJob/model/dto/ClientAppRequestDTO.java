ad4a73152e1e5dfd0ce4d1414abcadabf65d1cfd
sumflo <real61madrid@gmail.com>
2022-01-17 09:56:38 +0100

    ClientAppRequestDTO

---
+++ src/main/java/ksf/springboot/myDreamJob/model/dto/ClientAppRequestDTO.java
@@ -0,0 +1,11 @@
+package ksf.springboot.myDreamJob.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ClientAppRequestDTO {
+}
